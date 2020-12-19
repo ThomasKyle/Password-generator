92a6854806ed9a3180973f5863cad78dff7a67c4
ThomasKyle <thomas19992010@hotmail.com>
2020-12-18 19:57:23 -0500

    changed arrays

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,10 @@
 // Assignment code here
 
-// Array for the characters 
-const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
-const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
-const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57)
-const SYMBOLS_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(
-    arrayFromLowToHigh(58, 64 ).concat(
-        arrayFromLowToHigh(91, 96).concat(
-            arrayFromLowToHigh(123, 126)
-        )
-    )
-)
-
-// ask them how many characters they want to use
-var generatePassword = function(){
-    var promptlength = window.prompt("How many characters do you want to use?");
-
-    //validate prompt answer
-    if (promptlength <= 7 || >= 128){
-    window.alert("You're password is " + promptlength + " long.");
-    } 
-    else{
-        window.alert ("You're password must be between 8 - 128 characters.");
-    }
-
-    //confrim special characters and numbers 
-    var UPPERCASE_CHAR_CODES = window.confirm("Do you want to use Uppercase letters?");
-    var SYMBOLS_CHAR_CODES = window.confrim ("Do you want to use Symbols?");
-    var NUMBER_CHAR_CODES = window.confrim("Do you want to use Numbers?");
-}
-
-if (confrimUPPERCASE_CHAR_CODES){
-passwordCharacters = passwordCharacters.concat(UPPERCASE_CHAR_CODES)
-}
-
-if (confrimSYMBOLS_CHAR_CODES){
-    passwordCharacters = passwordCharacters.concat(SYMBOLS_CHAR_CODES)
-}
-
-if (confrimNUMBER_CHAR_CODES){
-    passwordCharacters = passwordCharacters.concat(NUMBER_CHAR_CODES)
-}
-
-console.log(passwordCharacters)
+//Arrays for the password generator
+var lowerCase = ("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
+var upperCase = ("A", "B", "C", "D", "E", "F","G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
+var number = ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0");
+var specialCharacter = ("!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~")
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
